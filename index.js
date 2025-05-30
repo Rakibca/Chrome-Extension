a434e843c575ab24838255717e16d7176ac2742f
Rakibul Islam <rakib.ca@gmail.com>
2025-05-30 00:10:59 -0600

    used localStorage

--- index.js
+++ index.js
@@ -3,6 +3,23 @@ const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
+// localStorage.setItem("myName", "Per Harald Borgen")
+
+let name = localStorage.getItem("myName");
+console.log(name);
+
+// localStorage.clear()
+
+// 1. Save a key-value pair in localStorage
+// 2. Refresh the page. Get the value and log it to the console
+// 3. Clear localStorage
+
+// HINTS:
+// localStorage.setItem(key, value)
+// localStorage.getItem(key)
+// localStorage.clear()
+// PS: both key and value need to be strings
+
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   inputEl.value = "";
@@ -12,8 +29,6 @@ inputBtn.addEventListener("click", function () {
 function renderLeads() {
   let listItems = "";
   for (let i = 0; i < myLeads.length; i++) {
-    // Refactor the code below to use a template string
-    // listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
     listItems += `
             <li>
                 <a target='_blank' href='${myLeads[i]}'>
